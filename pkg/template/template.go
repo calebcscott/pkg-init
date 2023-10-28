923dd1ee2bfc796243b1645d3067052733276f6b
Caleb C. Scott <calebcscott@outlook.com>
2023-10-28 15:09:22 -0500

    Extract command and template validation/creation

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -3,6 +3,10 @@ package template
 import (
 	"errors"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/calebcscott/pkg-init/pkg/config"
@@ -10,25 +14,112 @@ import (
 	//"gopkg.in/yaml.v3"
 )
 
-
+// main interface for interacting with template
 type template interface {
-    parse( *config.PkgConfig, string ) error
+    build( *config.PkgConfig, string ) error
+}
+
+
+type cmd struct {
+    cmds []exec.Cmd
 }
 
 type templateContent struct {
     contents map[string]interface{}
+    commands cmd
+}
+
+type templatePath struct {
+    path string
+    commands cmd
+}
+
+type templateGit struct {
+    repo string
+    commands cmd
 }
 
 
-func validate_template(contentMap interface{}, tld string) error {
+func newCommand(cmds string) (cmd, error) {
+
+    var commands []exec.Cmd
+    
+    for _, line := range strings.Split(cmds, "\n") {
+        if line == "" {
+            continue
+        }
+        cmdString := strings.Split(line, " ")
+        commands = append(commands, *exec.Command(cmdString[0], cmdString[1:]...))
+
+    }
+
+    return cmd{ commands }, nil
+}
+func (c cmd) run (dir string) error {
+
+    if len(c.cmds) == 0 {
+        return nil
+    }
+
+    for _, command := range c.cmds {
+        command.Dir = dir
+        var out strings.Builder
+        command.Stdout = &out
+        err := command.Run()
+
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Print(out.String())
+    }
+
+    return nil
+}
+
+func newTemplateContent(temp map[string]interface{}) (templateContent, error) {
+
+    // pull out contents
+    contents, found := temp["contents"]
+
+    if !found {
+        return templateContent{}, errors.New("malformed template, no contents found")
+    }
+    if err:= validate_template(contents); err != nil {
+        return templateContent{}, err
+    }
+
+    // pull out commands, optional field
+    commands := temp["commands"]
+
+    if commands == nil {
+        commands = ""
+    }
+    cmd, err := newCommand(commands.(string))
+    if err != nil {
+        return templateContent{}, err
+    }
+
+
+    return templateContent{contents.(map[string]interface{}), cmd }, nil
+}
+
+
+/*
+    validate_template
+
+    Validate whether supplied template matches expected contents
+    Do not need to pass a Top-Level-Directory / Prefix since we
+        do not do any path validation, only template validation
+*/
+func validate_template(contentMap interface{}) error {
     switch  contentMap := contentMap.(type) {
+    // empty case for string []interface{} since we expect those
     case string:
     case []interface{}:
     case map[string]interface{}:
-        for dirName, contents := range contentMap {
-            newTld := tld + "/" + dirName
-            
-            validate_template(contents, newTld)
+        for _, contents := range contentMap {
+            validate_template(contents)
         }
     default:
         return errors.New("malformed template")
@@ -37,44 +128,66 @@ func validate_template(contentMap interface{}, tld string) error {
     return nil
 }
 
+/*
+    build_template
 
-func build_template(contentMap interface{}, tld string) {
+    Still need to return possible errors with directory or file names/creation
+*/
+func build_template(contentMap interface{}, tld string) error {
+    var err error = nil
     switch  contentMap := contentMap.(type) {
     case string:
         // create file and add contents
         // filename will be tld, contents will be contentMap
+        err = os.WriteFile(tld, []byte(contentMap), 0644 )
+        if err != nil {
+            fmt.Println("Could not create file:", tld)
+        }
     case []interface{}:
         // empty dir, just need to create it
+        err = os.MkdirAll(tld, 0755)
     case map[string]interface{}:
         // possible dir/subdir/file
         for dirName, contents := range contentMap {
-            newTld := tld + "/" + dirName
-
+            // appends next path name with OS specific separator
+            newTld := filepath.Join(tld, dirName)
+            var dirErr error = nil
             // if match on switch create directory
             switch contents.(type) {
             case map[string]interface{}:
-                    // if subdirs or files, need to create this dir
-                    fmt.Println("Got dir to make", newTld)
-                
+                // if subdirs or files, need to create this dir
+                fmt.Println("Making directory:", newTld)
+                dirErr = os.MkdirAll(newTld, 0755)
+                if err == nil {
+                    err = dirErr
+                }
             }
 
-            // recurse to pick up files/subdirs
-            build_template(contents, newTld)
+            // recurse to pick up files/subdirs if no error
+            // cannot recurse if error'd on dir creation
+            if dirErr == nil {
+                subErr := build_template(contents, newTld)
+
+                if err == nil {
+                    err = subErr
+                }
+            } 
         }
     }
 
-
+    return err
 }
 
-func (t templateContent) parse( config *config.PkgConfig, tld string ) error {
-    fmt.Println("Parsing template" ) 
-
-    // first parse to validate
-    if err := validate_template(t.contents, tld); err != nil {
+func (t templateContent) build( config *config.PkgConfig, tld string ) error {
+    // attempt to build template
+    if err := build_template(t.contents, tld); err != nil {
         return err
     }
 
-    build_template(t.contents, tld)
+
+    if err := t.commands.run(tld); err != nil {
+        return err
+    }
 
     return nil
 }
@@ -100,11 +213,7 @@ func readTeamplate(templateName string, config *config.PkgConfig) (template, err
 
         switch t {
         case "raw":
-            if contents, found := templateMap["contents"]; found {
-                return templateContent{ contents.(map[string]interface{}) }, nil
-            } else {
-                return nil, errors.New("malformed template content")
-            }
+            return newTemplateContent(templateMap)
         }
     }
 
