84a8c826a849479624dfe045a0b6ad7ce183f48c
Caleb C. Scott <calebcscott@outlook.com>
2023-10-28 15:10:26 -0500

    Add user input on non-empty directories
    Prevent overriding/injecting template into non-empty directory without explicit direction from user

--- pkg/template/handler.go
+++ pkg/template/handler.go
@@ -2,7 +2,8 @@ package template
 
 import (
 	"fmt"
-
+    "io"
+    "os"
 	"github.com/calebcscott/pkg-init/pkg/config"
 )
 
@@ -15,10 +16,56 @@ type TemplateHandler struct {
 }
 
 
+func IsEmpty(name string) (bool, error) {
+    f, err := os.Open(name)
+    if err != nil {
+        return false, err
+    }
+
+    defer f.Close()
+
+    _, err = f.Readdirnames(1)
+    if err == io.EOF {
+        return true, nil
+    }
+
+    return false, err
+}
+
+func getChoiceB(msg string) bool {
+
+    for {
+        fmt.Print(msg)
+
+        var choice string
+        fmt.Scan(&choice)
+        switch  choice {
+        case "Y", "y":
+            return true
+        case "N", "n":
+            return false
+        default:
+            msg = "Must select yes or not [Y/n]: "
+        }
+    }
+}
+
 func (th TemplateHandler) Init(config *config.PkgConfig) error {
     fmt.Println("Initializing template", th.Name, "in directory", th.directory)
 
-    th.template.parse(config, th.directory)
+    empty, err := IsEmpty(th.directory)
+
+    if err == nil && !empty {
+        // need to check if user is Ok with starting in non-empty directory
+        msg := fmt.Sprintf("Directory %s is not empty, do you want to continue [Y/n]: ", th.directory)
+        if !getChoiceB(msg) {
+            return nil
+        }
+    }
+
+    if err := th.template.build(config, th.directory); err != nil {
+        return err
+    }
 
     return nil
 }
@@ -31,7 +78,6 @@ func NewTemplate(name string, lang string, dir string, config *config.PkgConfig)
         return TemplateHandler{}, error
     }
 
-    fmt.Println(template)
     return TemplateHandler{
         Name: name,
         language: lang,
