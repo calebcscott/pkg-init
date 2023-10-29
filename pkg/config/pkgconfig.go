2a84423e28d5da0faec9bcdaa3826d1eb6f55569
Caleb Scott <calebcscott@outlook.com>
2023-10-28 20:41:49 -0500

    Add language support
    Allow users to specify a particular language for project
    this will search for a template to provide, otherwise
    use default template

--- pkg/config/pkgconfig.go
+++ pkg/config/pkgconfig.go
@@ -8,16 +8,19 @@ import (
 type PkgConfig struct {
     CacheDir string
     TemplateMap map[string]string
+    LanguageSupport map[string]string
 }
 
 
 func NewConfig() PkgConfig {
     cache := viper.GetString("data-dir")
     templateMap := viper.GetStringMapString("templates")
+    langs := viper.GetStringMapString("languages")
 
     return PkgConfig{
         CacheDir: cache,
         TemplateMap: templateMap,
+        LanguageSupport: langs,
     }
 }
 
