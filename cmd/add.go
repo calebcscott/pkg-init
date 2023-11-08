40c565759c2907d145c2a6de64225fedbcb23c1d
Caleb Scott <calebcscott@outlook.com>
2023-11-07 22:02:09 -0600

    Add argument parsing and config update
    Parse arguments for template provided, build template name
    from provided file name. Provide choice to user if that want
    to override a particular template option that's already been
    set in the config. Lastly update config.

--- cmd/add.go
+++ cmd/add.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
 	"github.com/calebcscott/pkg-init/pkg/config"
+	"github.com/calebcscott/pkg-init/pkg/template"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 
@@ -13,14 +19,39 @@ var addCmd = &cobra.Command{
     Args: cobra.ExactArgs(1),
     Run: func(cmd *cobra.Command, args []string) {
         //TODO add logic to add template 
+        templateName, _ := cmd.Flags().GetString("name")
+        _, fileName := filepath.Split(args[0])
+        if templateName == "" {
+            templateName = strings.Split(fileName, ".")[0]
+        }
+        lang, _ := cmd.Flags().GetString("lang")
+
+        fmt.Println("Adding template:", templateName)
+        fmt.Println("for language type:", lang)
 
         // get viper configurations(?)
-        _ = config.NewConfig()
+        config := config.NewConfig()
 
+        // attempt to copy/cache template file
+        dstPath := template.CacheTemplateFile(args[0], &config)
 
         // parse/validate provided template
 
+        
         // add template to config with viper
+        v := viper.GetString("templates."+templateName)
+        if v != "" {
+            if !template.GetChoiceB("Overrideing template: "+v+", Do you want to continue?[Y/n]") {
+                return
+            }
+
+        }
+        viper.Set("templates."+templateName, dstPath)
+
+        err := viper.WriteConfig()
+        if err != nil {
+            fmt.Println("Error writing config")
+        }
     },
 }
 
@@ -31,4 +62,5 @@ func init() {
     // TODO add name flag, this may have to be done after adjustment to yaml file reading
     //  to allow name of template to be different from name of file
     addCmd.Flags().StringP("name", "n", "", "Name for template") 
+    addCmd.Flags().StringP("lang", "l", "", "Language/Project type for template") 
 }
