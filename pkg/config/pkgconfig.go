e0562cbb2f77ea40b8b8e9ed6f6e389d7959cc19
Caleb Scott <calebcscott@outlook.com>
2023-11-08 19:27:12 -0600

    Add logic to handle env vars/sring replacement on config item
    If config contains environment variables for cache dir attempt to substitute
    if cache dir not specified attempt to retrieve user's cache dir, otherwise
    just default to ~/.cache

--- pkg/config/pkgconfig.go
+++ pkg/config/pkgconfig.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -13,7 +18,28 @@ type PkgConfig struct {
 
 
 func NewConfig() PkgConfig {
+    var err error
+
+
     cache := viper.GetString("data-dir")
+    if cache != "" {
+        home := os.Getenv("HOME")
+        replacer := strings.NewReplacer("~", home, "${home}", home)
+        cache = replacer.Replace(cache)
+
+    } else {
+        cache, err = os.UserCacheDir()
+        if err != nil {
+            fmt.Println("Could not find suitable cache, defaulting to ~/.cache")
+            cache = "~/.cache"
+        }
+
+        cache = filepath.Join(cache, "pkg-init")
+    }
+    if _, err := os.Stat(cache); err != nil {
+        fmt.Println("Attempting to ensure cache dir exists: ", cache)
+        os.MkdirAll(cache, 0755)
+    }
     templateMap := viper.GetStringMapString("templates")
     langs := viper.GetStringMapString("languages")
 
